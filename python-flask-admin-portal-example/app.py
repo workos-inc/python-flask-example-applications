6b322006437792b5cbdb15b0b460cb43f4a426ef
Peter Arzhintar <pantera@workos.com>
2024-08-01 17:30:28 -0700

    Update sample for 5.0

--- python-flask-admin-portal-example/app.py
+++ python-flask-admin-portal-example/app.py
@@ -1,10 +1,15 @@
 import os
-
+from typing import TypeGuard, get_args
 from flask import Flask, redirect, render_template, request, url_for
 import workos
 from workos import client as workos_client
-from workos import portal
 from flask_lucide import Lucide
+import workos.organizations
+import workos.portal
+import workos.resources
+import workos.resources.organizations
+import workos.resources.portal
+import workos.resources.sso
 
 
 # Flask Setup
@@ -14,8 +19,14 @@ lucide = Lucide(app)
 
 # WorkOS Setup
 workos.api_key = os.getenv("WORKOS_API_KEY")
-workos.project_id = os.getenv("WORKOS_CLIENT_ID")
+workos.client_id = os.getenv("WORKOS_CLIENT_ID")
 workos.base_api_url = "http://localhost:7000/" if DEBUG else workos.base_api_url
+
+
+def is_portal_link_intent(
+    value: str,
+) -> TypeGuard[workos.portal.PortalLinkIntent]:
+    return value in get_args(workos.portal.PortalLinkIntent)
 
 
 @app.route("/")
@@ -32,15 +43,25 @@ def provision_enterprise():
 
     # Check if a matching domain already exists and set global org_id if there is a match
     orgs = workos_client.organizations.list_organizations(domains=organization_domains)
-    if len(orgs["data"]) > 0:
-        org_id = orgs["data"][0]["id"]
+    if len(orgs.data) > 0:
+        org_id = orgs.data[0].id
 
     # Otherwise create a new Organization and set the global org_id
     else:
-        organization = workos_client.organizations.create_organization(
-            {"name": organization_name, "domains": organization_domains}
+        domain_data = list(
+            map(
+                lambda domain: workos.organizations.DomainDataInput(
+                    {"domain": domain, "state": "verified"}
+                ),
+                organization_domains,
+            )
         )
-        org_id = organization["id"]
+
+        organization = workos_client.organizations.create_organization(
+            name=organization_name,
+            domain_data=domain_data,
+        )
+        org_id = organization.id
 
     return render_template("org_logged_in.html")
 
@@ -48,5 +69,13 @@ def provision_enterprise():
 @app.route("/launch_admin_portal", methods=["GET", "POST"])
 def launch_admin_portal():
     intent = request.args.get("intent")
-    portal_link = workos_client.portal.generate_link(organization=org_id, intent=intent)
-    return redirect(portal_link["link"])
+    if intent is None:
+        return "Missing intent parameter", 400
+
+    if not is_portal_link_intent(intent):
+        return "Invalid intent parameter", 400
+
+    portal_link = workos_client.portal.generate_link(
+        organization_id=org_id, intent=intent
+    )
+    return redirect(portal_link.link)
