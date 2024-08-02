1328d22c9ef7e4aac50b61654724ffc538882b60
Peter Arzhintar <pantera@workos.com>
2024-08-02 16:20:48 -0700

    Update audit logs app for 5.0

--- python-flask-audit-logs-example/app.py
+++ python-flask-audit-logs-example/app.py
@@ -1,12 +1,12 @@
 import json
 import os
-from urllib.parse import urlparse, parse_qs
-from flask import Flask, session, redirect, render_template, request, url_for
+from flask import Flask, session, redirect, render_template, request
 import workos
 from datetime import datetime, timedelta
 from audit_log_events import (
     user_organization_set,
 )
+from workos.audit_logs import AuditLogEvent
 from flask_lucide import Lucide
 
 
@@ -20,7 +20,7 @@ lucide = Lucide(app)
 
 # WorkOS Setup
 workos.api_key = os.getenv("WORKOS_API_KEY")
-workos.project_id = os.getenv("WORKOS_CLIENT_ID")
+workos.client_id = os.getenv("WORKOS_CLIENT_ID")
 workos.base_api_url = "http://localhost:7000/" if DEBUG else workos.base_api_url
 
 
@@ -35,13 +35,13 @@ app.jinja_env.filters["tojson_pretty"] = to_pretty_json
 def index():
     try:
         link = workos.client.portal.generate_link(
-            organization=session["organization_id"], intent="audit_logs"
+            organization_id=session["organization_id"], intent="audit_logs"
         )
         today = datetime.today()
         last_month = today - timedelta(days=30)
         return render_template(
             "send_events.html",
-            link=link["link"],
+            link=link.link,
             organization_id=session["organization_id"],
             org_name=session["organization_name"],
             last_month_iso=last_month.isoformat(),
@@ -51,13 +51,13 @@ def index():
         before = request.args.get("before")
         after = request.args.get("after")
         organizations = workos.client.organizations.list_organizations(
-            before=before, after=after, limit=5, order=None
+            before=before, after=after, limit=5, order="desc"
         )
-        before = organizations["listMetadata"]["before"]
-        after = organizations["listMetadata"]["after"]
+        before = organizations.list_metadata.before
+        after = organizations.list_metadata.after
         return render_template(
             "login.html",
-            organizations=organizations["data"],
+            organizations=organizations.data,
             before=before,
             after=after,
         )
@@ -65,14 +65,14 @@ def index():
 
 @app.route("/set_org", methods=["POST", "GET"])
 def set_org():
-    organization_id = request.args.get("id")
+    organization_id = request.args.get("id") or request.form["organization_id"]
+
     session["organization_id"] = organization_id
     organization_set = workos.client.audit_logs.create_event(
         organization_id, user_organization_set
     )
     org = workos.client.organizations.get_organization(organization_id)
-    org_name = org["name"]
-    session["organization_name"] = org_name
+    session["organization_name"] = org.name
     return redirect("/")
 
 
@@ -87,27 +87,29 @@ def send_event():
     )
     organization_id = session["organization_id"]
 
-    event = {
-        "action": "user.organization_deleted",
-        "version": int(event_version),
-        "occurred_at": datetime.now().isoformat(),
-        "actor": {
-            "type": actor_type,
-            "name": actor_name,
-            "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
-        },
-        "targets": [
-            {
-                "type": target_type,
-                "name": target_name,
-                "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
+    event = AuditLogEvent(
+        {
+            "action": "user.organization_deleted",
+            "version": int(event_version),
+            "occurred_at": datetime.now().isoformat(),
+            "actor": {
+                "type": actor_type,
+                "name": actor_name,
+                "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
             },
-        ],
-        "context": {
-            "location": "123.123.123.123",
-            "user_agent": "Chrome/104.0.0.0",
-        },
-    }
+            "targets": [
+                {
+                    "type": target_type,
+                    "name": target_name,
+                    "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
+                },
+            ],
+            "context": {
+                "location": "123.123.123.123",
+                "user_agent": "Chrome/104.0.0.0",
+            },
+        }
+    )
     organization_set = workos.client.audit_logs.create_event(organization_id, event)
     return redirect("/")
 
@@ -148,14 +150,14 @@ def get_events():
         try:
 
             create_export_response = workos.client.audit_logs.create_export(
-                organization=organization_id,
+                organization_id=organization_id,
                 range_start=request.form["range-start"],
                 range_end=request.form["range-end"],
                 actions=actions,
-                actors=actors,
+                actor_names=actors,
                 targets=targets,
             )
-            session["export_id"] = create_export_response.to_dict()["id"]
+            session["export_id"] = create_export_response.id
 
             return redirect("export_events")
         except Exception as e:
@@ -164,16 +166,23 @@ def get_events():
     if event_type == "access_csv":
         export_id = session["export_id"]
         fetch_export_response = workos.client.audit_logs.get_export(export_id)
-        return redirect(fetch_export_response.to_dict()["url"])
+        if fetch_export_response.url is None:
+            return redirect("/")
+
+        return redirect(fetch_export_response.url)
 
 
 @app.route("/events", methods=["GET"])
 def events():
+    intent = request.args.get("intent")
+    if not intent == "audit_logs":
+        return redirect("/")
+
     link = workos.client.portal.generate_link(
-        organization=session["organization_id"], intent=request.args.get("intent")
+        organization_id=session["organization_id"], intent=intent
     )
 
-    return redirect(link["link"])
+    return redirect(link.link)
 
 
 @app.route("/logout")
--- python-flask-audit-logs-example/audit_log_events.py
+++ python-flask-audit-logs-example/audit_log_events.py
@@ -1,20 +1,23 @@
 from datetime import datetime
+from workos.audit_logs import AuditLogEvent
 
-user_organization_set = {
-    "action": "user.organization_set",
-    "occurred_at": datetime.now().isoformat(),
-    "actor": {
-        "type": "user",
-        "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
-    },
-    "targets": [
-        {
-            "type": "team",
-            "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
+user_organization_set = AuditLogEvent(
+    {
+        "action": "user.organization_set",
+        "occurred_at": datetime.now().isoformat(),
+        "actor": {
+            "type": "user",
+            "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
         },
-    ],
-    "context": {
-        "location": "123.123.123.123",
-        "user_agent": "Chrome/104.0.0.0",
-    },
-}
+        "targets": [
+            {
+                "type": "organization",
+                "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
+            },
+        ],
+        "context": {
+            "location": "123.123.123.123",
+            "user_agent": "Chrome/104.0.0.0",
+        },
+    }
+)
